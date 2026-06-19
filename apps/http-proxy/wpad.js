5fa98752f0812110adc22b23545be8e71659380a
Aron Tsang <arontsang@gmail.com>
2026-06-20 05:49:57 +0800

    Update wpad to new namespace

--- apps/http-proxy/wpad.js
+++ apps/http-proxy/wpad.js
@@ -4,95 +4,95 @@ function FindProxyForURL(url, host) {
     }
 
     if (host === "junie.jetbrains.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
 
     if (dnsDomainIs(host, ".changelly.com") || host === "changelly.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
 
 
     if (dnsDomainIs(host, ".jetbrains.ai")) {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
 
 
     // Claude
     if (dnsDomainIs(host, ".claude.com") || host === "claude.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (dnsDomainIs(host, ".claude.ai") || host === "claude.ai") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (dnsDomainIs(host, ".anthropic.com") || host === "anthropic.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
 
 
     //BBC
     if (dnsDomainIs(host, ".bbc.com") || host === "bbc.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (host === "vod-dash-uk-live.akamaized.net" || host == "vod-thumb-uk-live.akamaized.net") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (dnsDomainIs(host, ".devices.a2z.com")) {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (dnsDomainIs(host, ".uk")) {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (host === "vs-hls-push-uk-live.akamaized.net" || host === "vs-hls-pushb-uk-live.akamaized.net") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (host === "vs-hls-push-uk-live.akamaized.net" || host === "vs-hls-pushb-uk-live.akamaized.net") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (host === "vs-cmaf-push-uk-live.akamaized.net") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (host === "bbcandroid.2cnt.net" ) {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
 
 
     if (dnsDomainIs(host, ".skycdp.com")) {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     //ITV
     if (dnsDomainIs(host, ".itv.com") || host === "itv.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (dnsDomainIs(host, ".itvx.com") || host === "itvx.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (dnsDomainIs(host, ".conviva.com") || host === "conviva.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (dnsDomainIs(host, ".launchdarkly.com") || host === "launchdarkly.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (shExpMatch(host, "itvpnp.*")) {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
 
     //Cruncy Roll
     if (dnsDomainIs(host, ".crunchyroll.com") || host === "crunchyroll.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (dnsDomainIs(host, ".ketchcdn.com") || host === "ketchcdn.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (host === "geolocation.onetrust.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
 
     //ChatGPT
     if (dnsDomainIs(host, ".chatgpt.com") || host === "chatgpt.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
     if (dnsDomainIs(host, ".openai.com") || host === "openai.com") {
-        return "PROXY socat.wpad.kube:3128";
+        return "PROXY uk-proxy.http-proxy.kube:3128";
     }
 
 
